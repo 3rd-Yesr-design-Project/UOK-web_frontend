f87033661a801320cd550931b714f0bbfbd4f1d8
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-04-30 20:20:31 +0530

    add to user profile

--- src/views/SocialMedia/profile/UserProfile.js
+++ src/views/SocialMedia/profile/UserProfile.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import UOK from '../../../assets/uok.jpg';
-import ellon from '../../../assets/ellon.jpg';
+import TimelineHeader from '../profile/userProfile/TimeLineHeader';
+import Intro from '../profile/userProfile/Introduction';
+import CreatePost from '../profile/userProfile/CreatePost';
+import Posts from '../profile/userProfile/Post';
+import MainPost from '../profile/userProfile/MainPost';
+import Photos from '../profile/userProfile/Photos';
 import './styles.css';
 const UserProfile = () => {
   console.log('prrrrrifle');
   return (
-    <div className='mt-5 container'>
-      <img src={UOK} alt='cover photo' className='mt-3 cover-photo' />
-      <div className='profile-photo'>
-        <img src={ellon} alt='profile photo' />
+    <div className='antialiased'>
+      <TimelineHeader />
+
+      <div className='px-52 grid grid-cols-12 pt-4 gap-4 bg-fFill z-0 pb-56'>
+        <div className='col-span-5 col-start-1 row-start-1 space-y-4'>
+          <Intro />
+          <Photos />
+        </div>
+        <div className='flex-row row-start-1 col-span-7 col-start-6 space-y-4'>
+          <CreatePost />
+          <Posts />
+          <MainPost />
+        </div>
       </div>
-      <p>sdfffffffffffffffffffffffffffffffff</p>
     </div>
   );
 };
