fe55e47374703ce90fed02d97babae5d485fbd68
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-04 22:52:14 +0530

    create student Action

---
+++ src/Action/studentAction.js
@@ -0,0 +1,20 @@
+import { GET_STUDENTS_SUCCESS, GET_STUDENTS_FAILED } from './types';
+
+export const getStudentByStudentIdndAcadomicYear = (payload) => async (
+  dispatch
+) => {
+  try {
+    dispatch({
+      type: GET_STUDENTS_SUCCESS,
+      payload: payload,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_STUDENTS_FAILED,
+      payload: {
+        msg: error?.response?.statusText,
+        status: error?.response?.status,
+      },
+    });
+  }
+};
