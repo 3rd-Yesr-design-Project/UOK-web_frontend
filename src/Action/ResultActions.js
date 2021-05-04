e50c61dcc3aeafe5c16f33549ec3ef418f48eafa
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-04 22:53:52 +0530

    modify result action

--- src/Action/ResultActions.js
+++ src/Action/ResultActions.js
@@ -4,11 +4,8 @@ import {
   ADD_RESULT_SUCCESS,
   ADD_RESULT_FAILED,
   STUDENT_RESULT,
-  GET_SUBJECTS_SUCCESS,
-  GET_SUBJECTS_FAILED,
   GET_STUDENTS_SUCCESS,
   GET_STUDENTS_FAILED,
-  GET_STUDENT_FAILED,
 } from './types';
 
 export const addResult = ({ data }) => async (dispatch) => {
@@ -53,40 +50,4 @@ export const fetchResultByUserIdAndYear = (payload) => {
     type: STUDENT_RESULT,
     payload: payload,
   };
-};
-
-export const getSubjectByAcadamicYear = (payload) => async (dispatch) => {
-  try {
-    dispatch({
-      type: GET_SUBJECTS_SUCCESS,
-      payload: payload,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_SUBJECTS_FAILED,
-      payload: {
-        msg: error?.response?.statusText,
-        status: error?.response?.status,
-      },
-    });
-  }
-};
-
-export const getStudentByStudentIdndAcadomicYear = (payload) => async (
-  dispatch
-) => {
-  try {
-    dispatch({
-      type: GET_STUDENTS_SUCCESS,
-      payload: payload,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_STUDENTS_FAILED,
-      payload: {
-        msg: error?.response?.statusText,
-        status: error?.response?.status,
-      },
-    });
-  }
 };
