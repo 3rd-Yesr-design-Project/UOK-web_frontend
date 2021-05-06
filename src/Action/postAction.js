ac43324225aac92e6d1cefafc6501a7cca80c80b
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-06 23:09:15 +0530

    create post action

--- src/Action/postAction.js
+++ src/Action/postAction.js
@@ -1,35 +1,50 @@
 import { CREATE_POST_FAIL, CREATE_POST_SUCCESS } from './types';
 import postService from '../services/PostService';
-export const postAction = (post) => (dispatch) => {
-  const { title, body, url } = post;
 
-  // fetch('http://localhost:4000/post/createpost', {
-  //   method: 'post',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     //  Authorization: 'Bearer ' + localStorage.getItem('jwt'),
-  //   },
-  //   body: JSON.stringify({
-  //     title,
-  //     body,
-  //     url,
-  //   }),
-  // })
+// fetch('http://localhost:4000/post/createpost', {
+//   method: 'post',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     //  Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+//   },
+//   body: JSON.stringify({
+//     title,
+//     body,
+//     url,
+//   }),
+// })
 
-  postService
-    .createPost(post)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.error) {
-        console.log(data.error);
-        dispatch({ type: CREATE_POST_FAIL, payload: data.error });
-        // M.toast({html: data.error, classes:"#c62828 red darken-3" })
-      } else {
-        console.log('post success created');
-        dispatch({ type: CREATE_POST_SUCCESS, payload: data });
-      }
-    })
-    .catch((error) => {
-      dispatch({ type: CREATE_POST_FAIL, payload: error });
+//   postService
+//     .createPost(post)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.error) {
+//         console.log(data.error);
+//         dispatch({ type: CREATE_POST_FAIL, payload: data.error });
+//         // M.toast({html: data.error, classes:"#c62828 red darken-3" })
+//       } else {
+//         console.log('post success created');
+//         dispatch({ type: CREATE_POST_SUCCESS, payload: data });
+//       }
+//     })
+//     .catch((error) => {
+//       dispatch({ type: CREATE_POST_FAIL, payload: error });
+//     });
+// };
+
+export const createPost = (payload) => async (dispatch) => {
+  try {
+    dispatch({
+      type: CREATE_POST_SUCCESS,
+      payload: payload.data.data,
     });
+  } catch (error) {
+    dispatch({
+      type: CREATE_POST_FAIL,
+      payload: {
+        msg: error?.response?.statusText,
+        status: error?.response?.status,
+      },
+    });
+  }
 };
