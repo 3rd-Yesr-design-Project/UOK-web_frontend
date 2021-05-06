737aef4da0285dc212875f052d1be54ea73f95c3
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-06 20:48:29 +0530

    modify post service

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -13,3 +13,33 @@
 // }
 
 // export default new PostService();
+
+import httpService from './HttpScicalService';
+
+class PostService {
+  createPost(payload) {
+    console.log('post service', payload);
+    let url;
+    const data = new FormData();
+    data.append('file', payload.image);
+
+    data.append('upload_preset', 'insta-clone');
+    data.append('cloud_name', 'ddeg8sl19');
+    fetch('https://api.cloudinary.com/v1_1/ddeg8sl19/image/upload', {
+      method: 'post',
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        url = data.url;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    payload.post_url = url;
+
+    return httpService.post('/post', payload);
+  }
+}
+
+export default new PostService();
