fc7ba5fd4723de765b8ec33226341b17b559dfb4
SahanHasintha <sahanhasintha12345@gmail.com>
2021-05-06 18:23:34 +0530

    Message, MessageInput, MessageHeader, MessageContent components added.

--- src/views/SocialMedia/ChatApp/Chat/Chat.js
+++ src/views/SocialMedia/ChatApp/Chat/Chat.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { SideBar } from '../Sidebar/Sidebar';
-
+import  Message  from '../Message/Message';
 function Chat() {
     return (
         <Grid columns="equal">
             <SideBar />
             <Grid.Column className="messagepanel">
-                
+                <Message/>
             </Grid.Column>
 
             <Grid.Column width={3}>
