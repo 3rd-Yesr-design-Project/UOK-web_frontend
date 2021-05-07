9d0227523bdc7f4ee780582fadc59ccb886d0789
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-07 20:40:27 +0530

    create search service

---
+++ src/services/SearchService.js
@@ -0,0 +1,9 @@
+import httpSocialService from './HttpSocialService';
+
+class SocialSearch {
+  socialSearchInfo(payload) {
+    //   return httpSocialService.post('/', payload)
+  }
+}
+
+export default new SocialSearch();
