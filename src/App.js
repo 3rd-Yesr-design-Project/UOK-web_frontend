0d565b16746ca4e5e3e317a7a66480535b731c49
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-08 21:31:52 +0530

    add firebase initialization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import NavigationBar from './componet/NavBar';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './views/Home/Home';
@@ -14,13 +15,54 @@ import SocialHome from './views/SocialMedia/home/SocialHome';
 import UserProfile from './views/SocialMedia/profile/UserProfile';
 import About from './componet/socialMedia/profile/about/About';
 import ResetPassword from './componet/common/ResetPassword';
+import DashboardComponent from './views/SocialMedia/Chat/Dashboard/dashboard';
+import SignupComponent from './views/SocialMedia/Chat/Signup/signup';
+import LoginComponent from './views/SocialMedia/Chat/Login/login';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 function App() {
+  const [state, setState] = useState({
+    socialUser: null,
+  });
+  useEffect(() => {
+    fireBaseInitialize();
+  }, []);
+  const db = firebase.firestore();
+  const fireBaseInitialize = () => {
+    if (!firebase?.apps?.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyC8qUhD8FtqEtpugwwnENSu552lDlfAztY',
+        authDomain: 'social-media-c89da.firebaseapp.com',
+        databaseURL: 'https://social-media-c89da-default-rtdb.firebaseio.com',
+        projectId: 'social-media-c89da',
+        storageBucket: 'social-media-c89da.appspot.com',
+        messagingSenderId: '603423739288',
+        appId: '1:603423739288:web:c209e235a970e615496fed',
+        measurementId: 'G-GQ7S729HWH',
+      });
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
+  };
+
+  const loggedIn = (user) => setState({ socialUser: user });
+
   return (
     <div>
       <BrowserRouter>
         {/* <NavigationBar /> */}
         <Switch>
+          <Route exact path='/dashboard' component={DashboardComponent} />
+
+          <Route
+            exact
+            path='/signup'
+            loginFn={loggedIn}
+            component={SignupComponent}
+          />
+
+          <Route exact path='/login' component={LoginComponent} />
           <Route exact path='/' component={Home} />
           <Route exact path='/results/login' component={ResultLogin} />
           <Route exact path='/results/view' component={Result} />
