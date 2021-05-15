37914dcb382a1fd2021d3b1e719bde6754b08082
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-15 20:38:49 +0530

    add update profile in reducer

--- src/Reducer/profileReducer.js
+++ src/Reducer/profileReducer.js
@@ -2,6 +2,8 @@ import {
   GET_PROFILE,
   UPDATE_PROFILE_ERROR,
   UPDATE_PROFILE,
+  CREATE_PROFILE,
+  CREATE_PROFILE_FAILED,
 } from '../Action/types';
 
 const initialState = {
@@ -10,13 +12,16 @@ const initialState = {
 
 export default function (state = initialState, action) {
   const { type, payload } = action;
-
+  console.log('update redus', payload);
   switch (type) {
     case GET_PROFILE:
       return { ...state, userProfile: payload };
     case UPDATE_PROFILE:
       return { ...state, userProfile: payload };
+    case CREATE_PROFILE:
+      return { ...state, userProfile: payload };
     case UPDATE_PROFILE_ERROR:
+    case CREATE_PROFILE_FAILED:
       return {
         ...state,
         error: {
