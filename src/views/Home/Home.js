d4f216dd1c94acd0d3eb849deaf9ed881c1b34b3
anjana6 <anjanashakthi95@gmail.com>
2021-05-16 12:35:04 +0530

    remove unnessary code

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,6 +4,7 @@ import uok2 from '../../assets/uok2.jpg';
 import HomeLayout from '../../componet/layout/HomeLayout';
 
 const Home = () => {
+  console.log('xxxxxxxxxxxxxx');
   return (
     <HomeLayout>
       <div className='container-fluid bg-gray'>
