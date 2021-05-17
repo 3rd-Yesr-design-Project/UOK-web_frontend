262cf120a18d5c15dd8c36f44e666aa76e992a2c
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-17 17:09:41 +0530

    add add and remove friend request

--- src/componet/socialMedia/home/FriendRequest.js
+++ src/componet/socialMedia/home/FriendRequest.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
-const FriendRequest = ({ friendRequest }) => {
-  console.log('pending', friendRequest);
+import Avatar from '@material-ui/core/Avatar';
+import FriendService from '../../../services/FriendService';
+import { addFriend, removeFriendRequest } from '../../../Action/friendAction';
+const FriendRequest = ({ friendRequest, addFriend }) => {
+  const requestAccept = async (freq) => {
+    const body = {
+      status: 'accept',
+    };
+    const data = {
+      ...freq,
+      status: 'accept',
+    };
+
+    const result = await FriendService.acceptFriendRequest(body, freq?.id);
+
+    if (result?.data?.message === 'Updated') {
+      addFriend(data);
+    }
+  };
+
+  const rejectRequest = async (freq) => {
+    const result = await FriendService.removeFriendRequest(freq?.id);
+    if (result?.data?.message === 'Deleted') {
+      const body = {
+        ...freq,
+        status: 'reject',
+      };
+      removeFriendRequest(body);
+    }
+
+    console.log('result', result);
+  };
+
   return (
     <div
       className='card mr-0'
@@ -14,16 +45,27 @@ const FriendRequest = ({ friendRequest }) => {
           freq?.status === 'pending' && (
             <div className='card m-2' style={{ width: '300px' }}>
               <div className='mb-2 ml-1 '>
+                <Avatar />
                 <span className='mr-1'>{freq?.user?.name}</span>
                 <div style={{ width: '200px', float: 'right' }}>
                   <span className='p-2 pt-3'>
                     {' '}
-                    <Button variant='primary' size='sm' className='pt-1 pb-1'>
+                    <Button
+                      variant='primary'
+                      size='sm'
+                      className='pt-1 pb-1'
+                      onClick={() => requestAccept(freq)}
+                    >
                       Accept
                     </Button>
                   </span>
                   <span className='p-1'>
-                    <Button variant='primary' size='sm' className='pt-1 pb-1'>
+                    <Button
+                      variant='primary'
+                      size='sm'
+                      className='pt-1 pb-1'
+                      onClick={() => rejectRequest(freq)}
+                    >
                       Reject
                     </Button>
                   </span>
@@ -43,4 +85,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(FriendRequest);
+export default connect(mapStateToProps, { addFriend })(FriendRequest);
