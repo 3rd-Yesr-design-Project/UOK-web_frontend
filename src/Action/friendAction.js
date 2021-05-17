d781aeb2703c9b6726a65b0e71de48d0ad75ceb9
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-05-17 17:09:01 +0530

    create fetchfrien action

--- src/Action/friendAction.js
+++ src/Action/friendAction.js
@@ -3,9 +3,14 @@ import {
   ADD_FRIEND_FAILED,
   GET_FRIEND_REQUESTS,
   GET_FRIEND_REQUESTS_FAILED,
+  FETCH_FRIEND,
+  FETCH_FRIEND_FAILED,
+  REMOVE_FRIEND,
+  REMOVE_FRIEND_FAILED,
 } from './types';
 
 export const addFriend = (payload) => async (dispatch) => {
+  console.log('Accept request', payload);
   try {
     dispatch({
       type: ADD_FRIEND,
@@ -31,6 +36,40 @@ export const getFriendRequest = (payload) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: GET_FRIEND_REQUESTS_FAILED,
+      payload: {
+        msg: error?.response?.statusText,
+        status: error?.response?.status,
+      },
+    });
+  }
+};
+
+export const fetchFriend = (payload) => async (dispatch) => {
+  try {
+    dispatch({
+      type: FETCH_FRIEND,
+      payload: payload,
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_FRIEND_FAILED,
+      payload: {
+        msg: error?.response?.statusText,
+        status: error?.response?.status,
+      },
+    });
+  }
+};
+
+export const removeFriendRequest = (payload) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REMOVE_FRIEND,
+      payload: payload,
+    });
+  } catch (error) {
+    dispatch({
+      type: REMOVE_FRIEND_FAILED,
       payload: {
         msg: error?.response?.statusText,
         status: error?.response?.status,
