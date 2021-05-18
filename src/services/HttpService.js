f55357ed668d9d18dbdd6c72b838db0991397f11
anjana6 <anjanashakthi95@gmail.com>
2021-05-18 20:36:19 +0530

    change url to localhost

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpService = axios.create({
-  baseURL: 'http://34.203.13.248/api/v1',
+  baseURL: 'http://localhost:5000/api/v1',
 });
 
 httpService.interceptors.request.use((config) => {
